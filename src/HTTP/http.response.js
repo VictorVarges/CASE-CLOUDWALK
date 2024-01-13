0d84ef104b6001b084621b713d947ebde33df10e
Victor Varges <victorryandias@gmail.com>
2024-01-13 13:55:48 -0300

    using error middleware for validations, in addition to creating standardization of http response and status

--- src/HTTP/http.response.js
+++ src/HTTP/http.response.js
@@ -1,10 +1,21 @@
-// export enum HTTPSTATUS {
-//   OK = 200,
-//   CREATED = 201,
-//   UNAUTHORIZED = 401,
-//   BAD_REQUEST = 400,
-//   UNPROCESSABLE_ENTITY = 422,
-//   NOT_FOUND = 404,
-// }
+const HTTPSTATUS = {
+  OK : 200,
+  CREATED : 201,
+  UNAUTHORIZED : 401,
+  BAD_REQUEST : 400,
+  UNPROCESSABLE_ENTITY : 422,
+  NOT_FOUND : 404,
+}
 
-// module.exports = { HTTPSTATUS }
+const HTTPRESPONSE = {
+  transaction_id: 'field empty: Please, insert the field transaction_id',
+  merchant_id: 'field empty: Please, insert the field merchant_id' ,
+  user_id: 'field empty: Please, insert the field user_id',
+  card_number: 'field empty: Please, insert the field card_number',
+  transaction_date: 'field empty: Please, insert the field transaction_date',
+  transaction_amount: 'field empty: Please, insert the field transaction_amount', 
+  device_id: 'field empty: Please, insert the field device_id'
+}
+
+
+module.exports = { HTTPSTATUS, HTTPRESPONSE }
--- src/middlewares/validations.js
+++ src/middlewares/validations.js
@@ -1,5 +1,5 @@
-// const HTTPRESPONSE = require('../HTTP/http.response');
 const express = require('express');
+const { HTTPRESPONSE, HTTPSTATUS } = require('../HTTP/http.response')
 
 
 
@@ -7,25 +7,25 @@ const validationFieldRequired = (req, res, next) => {
   const payload = req.body
   console.log('AAAAAAAA', payload[0]);
   if (!payload.transaction_id) {
-    return res.status(422).json({ 'field empty': 'Please, insert the field transaction_id' })
+    return res.status(HTTPSTATUS.UNPROCESSABLE_ENTITY).json(HTTPRESPONSE.transaction_id)
   }
   if (!payload.merchant_id) {
-    return res.status(422).json({ 'field empty': 'Please, insert the field merchant_id' })
+    return res.status(HTTPSTATUS.UNPROCESSABLE_ENTITY).json(HTTPRESPONSE.merchant_id)
   }
   if (!payload.user_id) {
-    return res.status(422).json({ 'field empty': 'Please, insert the field user_id' })
+    return res.status(HTTPSTATUS.UNPROCESSABLE_ENTITY).json(HTTPRESPONSE.user_id)
   }
   if (!payload.card_number) {
-    return res.status(422).json({ 'field empty': 'Please, insert the field card_number' })
+    return res.status(HTTPSTATUS.UNPROCESSABLE_ENTITY).json(HTTPRESPONSE.card_number)
   }
   if (!payload.transaction_date) {
-    return res.status(422).json({ 'field empty': 'Please, insert the field transaction_date' })
+    return res.status(HTTPSTATUS.UNPROCESSABLE_ENTITY).json(HTTPRESPONSE.transaction_date)
   }
   if (!payload.transaction_amount) {
-    return res.status(422).json({ 'field empty': 'Please, insert the field transaction_amount' })
+    return res.status(HTTPSTATUS.UNPROCESSABLE_ENTITY).json(HTTPRESPONSE.transaction_amount)
   }
   if (!payload.device_id) {
-    return res.status(422).json({ 'field empty': 'Please, insert the field device_id' })
+    return res.status(HTTPSTATUS.UNPROCESSABLE_ENTITY).json(HTTPRESPONSE.device_id)
   }
 
   next();
